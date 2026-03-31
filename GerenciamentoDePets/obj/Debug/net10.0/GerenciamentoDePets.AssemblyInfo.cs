5601363ad70be8b80143a6223e5fb50c4dab0fb2
THIAGO ESPERIDIÃO MOURA RIBEIRO <49631963870@info.local>
2026-03-31 09:33:10 -0300

    Fazendo a DTO

--- GerenciamentoDePets/obj/Debug/net10.0/GerenciamentoDePets.AssemblyInfo.cs
+++ GerenciamentoDePets/obj/Debug/net10.0/GerenciamentoDePets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoDePets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d700ce46ae9429ab21e694297c550a09be5b617b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7c078c05705f6410a039830b6373f069fe8b43")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoDePets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoDePets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
